fd1d2bb37e7251d06e56cbba1b3eab7f2acd6c23
PC_evgenyi <abusyginza@gmail.com>
2025-07-11 07:48:20 +0700

    FSM_01

--- FSM/handler_FSM.py
+++ FSM/handler_FSM.py
@@ -1,3 +1,4 @@
+import email
 from aiogram import F, Router
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
@@ -14,8 +15,21 @@ handler = Router()
 
 class Survey(StatesGroup):
     full_name = State()
+    email = State()
 
 
+def validate_email(text: str):
+    if "@" not in text or "." not in text:
+        raise ValueError("Invalid email")
+    return text.lower()
+
+
+def valid_email_filter(message: Message):
+    try:
+        email = validate_email(message.text)
+    except ValueError:
+        return None
+    return {"email": email}
 
 
 @handler.message(Command('survey'))
@@ -27,15 +41,37 @@ async def handle_start(message: Message, state: FSMContext):
 
 
 @handler.message(Survey.full_name, F.text)
-async def handler_survey_user_full_name(message: Message):
+async def handler_survey_user_full_name(message: Message, state: FSMContext):
+    await state.update_data(full_name=message.text)
+    await state.set_state(Survey.email)
     await message.answer(
-        f"Hello {markdown.hbold(message.text)}",
+        f"Hello {markdown.hbold(message.text)}, now please share your email",
         parse_mode=ParseMode.HTML,
          
     )
 
 @handler.message(Survey.full_name)
-async def handle_survey_user_full_name_invalid_content_type(message: Message):
+async def handle_survey_user_full_name_invalid_content_type(message: Message, state: FSMContext):
+    await state.clear()
     await message.answer(
         "Sorry, I didn't understand, send your full name as text"
+    )
+
+
+@handler.message(Survey.email, valid_email_filter)
+async def handler_survey_user_email(
+    message: Message,
+    state: FSMContext,
+    email: str):
+    await state.update_data(email=message.text)
+    await message.answer(
+        text=f"Cool yuor email is now {markdown.hcode(email)}",
+    )
+
+
+@handler.message(Survey.email)
+async def handler_survey_invalid_email(
+    message: Message):
+    await message.answer(
+        text=f"Invalid email", 
     )
